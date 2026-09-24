1ea8d1f1f632b11214809441dbc8f124483751d8
Dharshini <dharshininata@gmail.com>
2026-09-24 10:08:19 +0530

    Test CI failure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
       <h1>Hello CI/CD</h1>
-      <p>This application is built automatically.</p>
+      <p>This application is built automatically.
     </div>
   );
 }
